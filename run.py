969c3d6d813e8fa9957568e5c28a9dbe918130f1
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-06-11 19:33:58 +0000

    add changes done

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import gspread
+
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
@@ -26,7 +26,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+     
 
         sales_data = data_str.split(",")
 
