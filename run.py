ac138ee33a58ef37451da93b1607aaddcec835ab
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-06-11 19:16:01 +0000

    add getting the last five entries

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -85,6 +86,22 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
 
     return surplus_data
+
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
